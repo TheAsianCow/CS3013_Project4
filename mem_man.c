b8103852d110521e8d17c65c7f5911cc455c6bad
Jeffrey Huang <jeffreyhuangdragon@gmail.com>
2020-02-25 17:16:21 -0500

    added typedef for unsigned char

--- mem_man.c
+++ mem_man.c
@@ -4,15 +4,10 @@
 #include <string.h>
 #include "mem_man.h"
 
-//error codes
-#define OUT_OF_BOUNDS 0x40
-#define ERR2 0x41
-#define ERR3 0x42
-//...
-
-unsigned char mem[64];
-int free_list[4];
-unsigned char proc_reg[4];//stores 
+addr mem[64];
+int free_list[4];//indices correspond to the pages, value stored at the index corresponds to the pid that's using the frame
+				 //-1 means that the page is free 
+addr proc_reg[4];//stores the physical address that points to the page table for the corresponding PID
 
 int main(int argc, char* argv[]){
 	int pid;
@@ -29,53 +24,61 @@ int main(int argc, char* argv[]){
     return 0;
 }
 
-int execute(int pid, char* instruction, unsigned char v_address, int val) {
+int execute(int pid, char* instruction, addr v_address, int val) {
 	printf("execute\n");
 }
 
-int allocate(int pid, char* instruction, unsigned char v_address, int val) {
+int allocate(int pid, char* instruction, addr v_address, int val) {
+	//first we need an empty page frame
+	int i;
+	for(i = 0; i < 4; i++){
+
+	}
 	printf("allocate\n");
 }
 
 //returns the physical address at which the value is stored at
-//returns 0 if failed
-int store(int pid, char* instruction, unsigned char v_address, int val) {
-	unsigned char phys_address = VPN_TO_MEM(pid, v_address);
+//returns -1 if failed
+int store(int pid, char* instruction, addr v_address, int val) {
+	addr phys_address = VPN_TO_MEM(pid, v_address);
 	if(phys_address>0x30){
-		err_handler(phys_address);
+		err_handler(phys_address, 0);
 		return -1; 
 	}
 	mem[phys_address] = val;
-	printf("Stored value %d at virtual address %d (physical address %d)\n", val, (int)v_address, (int)phys_address);
+	printf("Stored value %d at virtual address %d (physical address %d)\n", val, (unsigned int)v_address, (unsigned int)phys_address);
 	return phys_address;
 }
 
 //returns the value stored in memory at the virtual address
-int load(int pid, char* instruction, unsigned char v_address) {
-	unsigned char phys_address = VPN_TO_MEM(pid, v_address);
+//returns -1 if failed
+int load(int pid, char* instruction, addr v_address) {
+	addr phys_address = VPN_TO_MEM(pid, v_address);
 	if(phys_address>0x30){
-		err_handler(phys_address);
+		err_handler(phys_address, 0);
 		return -1;
 	}
 	int val = mem[phys_address]; 
-	printf("The value %d is at virtual address %d (physical address %d)\n", val, (int)v_address, (int)phys_address);
+	printf("The value %d is at virtual address %d (physical address %d)\n", val, (unsigned int)v_address, (unsigned int)phys_address);
 	return val;
 }
 
 //outputs the physical mem address of a given virtual address
-unsigned char VPN_TO_MEM(int pid, unsigned char address){
-	unsigned char VPN  = address&0x30;//gets the VPN from the bits 4 and 5 from the virtual address
-	unsigned char offset = address&0x0f;//get the offset which are the 4 lower bits
-	unsigned char PTE = mem[proc_reg[pid]+VPN];//get the first PTE from the address of the page table stored in the proc reg
+addr VPN_TO_MEM(int pid, addr address){
+	addr VPN  = address&0x30;//gets the VPN from the bits 4 and 5 from the virtual address
+	addr offset = address&0x0f;//get the offset which are the 4 lower bits
+	addr PTE = mem[proc_reg[pid]+VPN];//get the first PTE from the address of the page table stored in the proc reg
 	if(!PTE) return OUT_OF_BOUNDS;//error code since the virtual address doesn't have a PTE which means it is out of bounds 
-	unsigned char PFN = PTE&0x30;//the physical frame numbers are located at bits 4 and 5
+	addr PFN = PTE&0x30;//the physical frame numbers are located at bits 4 and 5
 	return PFN+offset;//adds the PFN back to the offset to get the address in the frame that has the data
 }
 
-void err_handler(unsigned char err){
+void err_handler(addr err, int err_val){
 	switch(err){
 		case OUT_OF_BOUNDS: 
 			printf("ERROR: virtual address outside of page boundaries\n");
+		case PAGE_OVERLAP:
+			printf("ERROR: virtual page already mapped into physical frame %d\n", err_val);
 		default: 
 			printf("ERROR: unknown error, tried to access physical memory address %d, which is larger than memory\n", (unsigned int)err);
 	}
--- mem_man.h
+++ mem_man.h
@@ -1,12 +1,20 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+//error codes
+#define OUT_OF_BOUNDS 0x40
+#define PAGE_OVERLAP 0x41
+#define ERR3 0x42
+//...
+
+typedef unsigned char addr;
+
 int main(int argc, char* argv[]);
-int execute(int pid, char* instruction, unsigned char v_address, int val);
-int allocate(int pid, char* instruction, unsigned char v_address, int val);
-int store(int pid, char* instruction, unsigned char v_address, int val);
-int load(int pid, char* instruction, unsigned char v_address);
-unsigned char VPN_TO_MEM(int pid, unsigned char address);
-void err_handler(unsigned char err);
+int execute(int pid, char* instruction, addr v_address, int val);
+int allocate(int pid, char* instruction, addr v_address, int val);
+int store(int pid, char* instruction, addr v_address, int val);
+int load(int pid, char* instruction, addr v_address);
+addr VPN_TO_MEM(int pid, addr address);
+void err_handler(addr err, int err_val);
 
 #endif
