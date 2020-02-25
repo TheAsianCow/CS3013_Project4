020e26797570b05c29840e9e044479883b062c8e
jwu2018 <jyalu.wu@hotmail.com>
2020-02-24 22:13:29 -0500

    added header and makefile

--- mem_man.c
+++ mem_man.c
@@ -2,10 +2,35 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include "mem_man.h"
 
 int main(int argc, char* argv[]){
-	
+	int pid;
+	char* instruction;
+	int v_address;
+	int val;
 
-	
-	return 0;
+	// command prompt
+	printf("Instruction? ");
+	scanf("%d %s %d %d", &pid, instruction, &v_address, &val);
+
+	printf("pid = %d, inst = %s, va = %d, val = %d\n", pid, instruction, v_address, val);
+    execute(pid, instruction, v_address, val);
+    return 0;
+}
+
+int execute(int pid, char* instruction, int v_address, int val) {
+	printf("execute\n");
+}
+
+int allocate(int pid, char* instruction, int v_address, int val) {
+	printf("allocate\n");
+}
+
+int store(int pid, char* instruction, int v_address, int val) {
+	printf("store\n");
+}
+
+int load(int pid, char* instruction, int v_address) {
+	printf("load\n");
 }
