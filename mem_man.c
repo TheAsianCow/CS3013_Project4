8664bfb4e235dd4cd064178e89f603596ffac396
Jeffrey Huang <jeffreyhuangdragon@gmail.com>
2020-02-24 22:13:58 -0500

    getting basic functions

--- mem_man.c
+++ mem_man.c
@@ -3,9 +3,19 @@
 #include <unistd.h>
 #include <string.h>
 
-int main(int argc, char* argv[]){
-	
+char mem[64];
+int free_list[4];
+char proc_reg[4];
 
-	
+int main(int argc, char* argv[]){
+
+
+
 	return 0;
+}
+
+//outputs the physical address of a function 
+char VPN_to_PFN(int pid, char address){
+	int offset = address&0b00001111;
+
 }
