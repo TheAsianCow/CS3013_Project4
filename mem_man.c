b960aa089e520e1e995a2ab2707844993efefa44
Jeffrey Huang <jeffreyhuangdragon@gmail.com>
2020-02-25 13:14:23 -0500

    getting latest changes

--- mem_man.c
+++ mem_man.c
@@ -33,13 +33,19 @@ int allocate(int pid, char* instruction, int v_address, int val) {
 
 int store(int pid, char* instruction, int v_address, int val) {
 	printf("store\n");
+	char phys_address = VPN_TO_PFN(pid, v_address);
+	mem[phys_address] = val;
+	return 1;
 }
 
 int load(int pid, char* instruction, int v_address) {
 	printf("load\n");
 }
 
-//outputs the physical address of a function 
-char VPN_to_PFN(int pid, char address){
-	int offset = address&0b00001111;
+//outputs the physical address of a given virtual address, which basically just gives you the index of the array
+//the first 2 bits of the array should always be 0, otherwise its automatically invalid since it exceeds the boundaries of the array
+char VPN_TO_PFN(int pid, char address){
+	char offset = address&0b00001111;//get the offset
+	char PFN = proc_reg[pid];
+	return PFN+offset;
 }
