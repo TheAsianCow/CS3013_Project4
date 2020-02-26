b6f4ae658aeb2956753051cf74c705c686c2dbb2
jwu2018 <jyalu.wu@hotmail.com>
2020-02-25 22:13:20 -0500

    user input

--- mem_man.c
+++ mem_man.c
@@ -10,29 +10,30 @@ int free_list[4];//indices correspond to the pages, value stored at the index co
 addr proc_reg[4];//stores the physical address that points to the page table for the corresponding PID
 
 int main(int argc, char* argv[]){
-	// char* pid_str;
+	char pid_str[5];
 	int pid;
 	char* instruction;
-	// char* v_ad_str;
+	char v_address_str[5];
 	unsigned char v_address;
-	// char* value_str;
+	char value_str[5];
 	int value;
-	// int correct_input = 0;
+	int correct_input = 0;
+	// char* args[4];
 
-	char* file_path = "instructions.txt";
-	char array[50];
-    char* line = array;
+	// char* file_path = "instructions.txt";
+	char line[50];
+    // char* line = array;
     char* args[50];
-    ssize_t size;
-    size_t n = 0;
-    int eof = 0;
+ //    ssize_t size;
+ //    size_t n = 0;
+ //    int eof = 0;
 
     // parsing
-	FILE* file = fopen(file_path,"r");
-	if(file == NULL) {
-      perror("Error opening file");
-      return(-1);
-	}
+	// FILE* file = fopen(file_path,"r");
+	// if(file == NULL) {
+ //      perror("Error opening file");
+ //      return(-1);
+	// }
     // size = getline(&line,&n,file);
     // printf("size = %d\n", size);
     
@@ -40,37 +41,62 @@ int main(int argc, char* argv[]){
    //     writing content to stdout 
    //    puts(str);
    // }
-    while(eof == 0){
+    while(correct_input == 0){
+    	printf("Instructions? ");
+    	// scanf("%d %s %c %d\n", &pid, instruction, &v_address, &value);
+    	fgets (line, 50, stdin);
+    	parse(line, args);
+    	printf("line: %s\n", line);
+    	// printf("%d\t%s\t%d\t%d\n", pid, instruction, v_address, value);
+    	if (isValidArgs(args)) {
+    		pid = atoi(args[0]);
+    		instruction = args[1];
+    		// printf("args 2 = %s\n", args[2]);
+    		v_address = (unsigned char) atoi(args[2]);
+    		value = atoi(args[3]);
+    		if (strcmp(instruction, "allocate") == 0) allocate(pid, instruction, v_address, value);
+			if (strcmp(instruction, "store") == 0) store(pid, instruction, v_address, value);
+			if (strcmp(instruction, "load") == 0) load(pid, instruction, v_address);
+			correct_input = 1;
+    	}
+    	else {
+    		printf("Your arguments were incorrect.\nThe correct format is: (process id, instruction, virtual address, value)\n");
+    		printf("\t- process id: int in range [0, 3]\n");
+    		printf("\t- instruction: either \"allocate\", \"store\", or \"load\"\n");
+    		printf("\t- virtual address: int in range [0, 63]\n");
+    		printf("\t- value: int in range [0, 255]\n");
+    	}
     	// printf("line = %s\n", line);
         // if(line[size-1]=='\n') line[size-1]='\0';
         // printf("while\n");
         // size = getline(&line, &n, file);
         // execute(line);
         // size = getline(&line,&n,file);
-        if (fgets(line, 50, file) != NULL){
-	        printf("line = %s\n", line);
-	        // split line into words and clean commas
-	        parse(line, args);
-	        // printf("%s\t%s\t%s\t%s\n", args[0], args[1], args[2], args[3]);
-	        // check if words are valid
-	        if (isValidArgs(args)) {
-	        	pid = atoi(args[0]);
-	        	instruction = args[1];
-	        	strcpy((char*) args[2], instruction);
-	        	value = atoi(args[3]);
+        
+    //     if (fgets(line, 50, file) != NULL){
+	   //      printf("line = %s\n", line);
+	   //      // split line into words and clean commas
+	   //      parse(line, args);
+	   //      // printf("%s\t%s\t%s\t%s\n", args[0], args[1], args[2], args[3]);
+	   //      // check if words are valid
+	   //      if (isValidArgs(args)) {
+	   //      	pid = atoi(args[0]);
+	   //      	instruction = args[1];
+	   //      	strcpy((char*) args[2], instruction);
+	   //      	value = atoi(args[3]);
 
-	        	printf("pid = %d\n", pid);
-	        	printf("instruction = %s\n", instruction);
-	        	printf("v_address = %d\n", v_address);
-	        	printf("value = %d\n", value);
+	   //      	printf("pid = %d\n", pid);
+	   //      	printf("instruction = %s\n", instruction);
+	   //      	printf("v_address = %d\n", v_address);
+	   //      	printf("value = %d\n", value);
 
-	        	if (strcmp(instruction, "allocate") == 0) allocate(pid, instruction, v_address, value);
-				if (strcmp(instruction, "store") == 0) store(pid, instruction, v_address, value);
-				if (strcmp(instruction, "load") == 0) load(pid, instruction, v_address);
-	        }
-	    }
-	    else
-	    	eof = 1;
+	   //      	if (strcmp(instruction, "allocate") == 0) allocate(pid, instruction, v_address, value);
+				// if (strcmp(instruction, "store") == 0) store(pid, instruction, v_address, value);
+				// if (strcmp(instruction, "load") == 0) load(pid, instruction, v_address);
+	   //      }
+	   //  }
+	   //  else
+	   //  	eof = 1;
     }
     // fclose(file_path);
     return 0;
@@ -83,24 +109,25 @@ int main(int argc, char* argv[]){
  * @param args, where it stores the split strings
  */
 void parse(char* line, char** args){
-     const char s[3] = ", ";
-     char* token = strtok(line, s);
-     int argc = 0;
-     while(token!=NULL){
-        args[argc] = strdup(token);
-        argc++;
-        token = strtok(NULL, s);
-     }
-     args[argc] = NULL;
+	printf("parse\n");
+	const char s[3] = ", ";
+	char* token = strtok(line, s);
+	int argc = 0;
+	while(token!=NULL){
+	args[argc] = strdup(token);
+	argc++;
+	token = strtok(NULL, s);
+	}
+	args[argc] = NULL;
 }
 
 int isValidArgs(char** args) {
 	int pid = atoi(args[0]);
 	char* instruction = args[1];
-	unsigned char v_address = (int) args[2];
+	unsigned char v_address = (unsigned char) atoi(args[2]);
 	int value = atoi(args[3]);
-	// printf("isValidArgs\n");
-	// printf("%d\t%s\t%d\t%d\n", pid, instruction, v_address, value);
+	printf("isValidArgs\n");
+	printf("pid = %d\tinstruction = %s\tv_address = %d\tvalue = %d\n", pid, instruction, v_address, value);
 	// printf(pid >= 0 && pid <= 3);
 	// printf(v_address >= 0 && v_address <= 63);
 	// printf()
